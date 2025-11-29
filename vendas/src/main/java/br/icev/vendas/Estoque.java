49310258b27fd2d156da66d11aaf9cbcbaa513d5
Absalão <absalaoresplandes@gmail.com>
2025-11-29 19:17:55 -0300

    implementanto-classe-Estoque

--- vendas/src/main/java/br/icev/vendas/Estoque.java
+++ vendas/src/main/java/br/icev/vendas/Estoque.java
@@ -2,18 +2,47 @@ package br.icev.vendas;
 
 import br.icev.vendas.excecoes.QuantidadeInvalidaException;
 import br.icev.vendas.excecoes.SemEstoqueException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Estoque {
+
+    private Map<String, Integer> itens = new HashMap<>();
+
     public void adicionarEstoque(String codigo, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade tem que ser maior que zero");
+        }
+
+        if (itens.containsKey(codigo)) {
+            int atual = itens.get(codigo);
+            itens.put(codigo, atual + quantidade);
+        } else {
+            itens.put(codigo, quantidade);
+        }
     }
 
     public int getDisponivel(String codigo) {
-        throw new UnsupportedOperationException("TODO");
+        if (itens.containsKey(codigo)) {
+            return itens.get(codigo);
+        }
+        return 0;
     }
 
     public void reservar(String codigo, int quantidade)
             throws SemEstoqueException, QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade inválida para reserva");
+        }
+
+        int disponivel = getDisponivel(codigo);
+
+        if (disponivel < quantidade) {
+            throw new SemEstoqueException("Estoque insuficiente para o produto " + codigo);
+        }
+
+        itens.put(codigo, disponivel - quantidade);
     }
 }
